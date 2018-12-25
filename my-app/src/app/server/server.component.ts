207e144266d71d700b617d893a9027f148f54b75
apiwat.h <apiwat.h@aware.co.th>
2018-12-25 14:21:21 +0700

    Section: ngStyle

--- my-app/src/app/server/server.component.ts
+++ my-app/src/app/server/server.component.ts
@@ -9,8 +9,17 @@ export class ServerComponent {
   serverId: Number = 10;
   serverStatus: String = 'offline';
 
+  constructor() {
+    this.serverId = Math.random();
+    this.serverId > 0.5 ? this.serverStatus = 'online' : this.serverStatus = 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'offline' ? 'red' : 'green';
   }
 
 }
