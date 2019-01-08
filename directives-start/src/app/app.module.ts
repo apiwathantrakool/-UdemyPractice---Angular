1a4d2b35f66ca9edf3848122fc2a950babcbd024
apiwat.h <apiwat.h@aware.co.th>
2019-01-08 15:37:47 +0700

    Section-07-87: Attribute element directive (Better Custom).

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from 'src/basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
