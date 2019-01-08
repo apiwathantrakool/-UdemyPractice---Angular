605703a36b3ccf3d33268a070bf9f4260f1f8808
apiwat.h <apiwat.h@aware.co.th>
2019-01-08 16:26:51 +0700

    Section-07-91: Attribute element directive (Using HostListener, HostBinding and @Input() ).

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,11 +1,14 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @Input() defaultBorder: String;
+  @Input() highlightBorder: String = 'dashed';
   @HostBinding('style.background-color') backgroundColor: String = 'transparent';
+  @HostBinding('style.border-style') borderStyle: String = this.defaultBorder;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
   ngOnInit() {
@@ -17,12 +20,14 @@ export class BetterHighlightDirective implements OnInit {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'black');
     this.backgroundColor = 'black';
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+    this.borderStyle = this.highlightBorder;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
     this.backgroundColor = 'transparent';
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
+    this.borderStyle = this.defaultBorder;
   }
 
 }
