6dfbf27b61cea569360bc63d97ae9b248e5faada
apiwat.h <apiwat.h@aware.co.th>
2019-01-08 14:46:40 +0700

    Just change some color.

--- directives-start/src/basic-highlight/basic-highlight.directive.ts
+++ directives-start/src/basic-highlight/basic-highlight.directive.ts
@@ -7,6 +7,6 @@ import { Directive, OnInit, ElementRef } from '@angular/core';
 export class BasicHighlightDirective implements OnInit {
     constructor(private elementRef: ElementRef) {}
     ngOnInit() {
-        this.elementRef.nativeElement.style.backgroundColor = 'green';
+        this.elementRef.nativeElement.style.backgroundColor = '#99ff66';
     }
 }
