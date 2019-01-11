b8ad91f0668f8e2334aebeb8d7c99e9f183e1e94
apiwat.h <apiwat.h@aware.co.th>
2019-01-11 10:55:37 +0700

    Section-13-164: Building the Custom Observable.

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs-compat';
+import { Observable, Observer } from 'rxjs-compat';
 // tslint:disable-next-line:import-blacklist
 import 'rxjs/Rx';
 
@@ -13,11 +13,70 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   }
+    // );
+
+// Subscribe with the data and error arguments.
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout(
+          () => {
+            observer.next('First package');
+          }, 2000
+        );
+
+        setTimeout(
+          () => {
+            observer.next('Second package');
+          }, 2000
+        );
+
+        setTimeout(
+          () => {
+            observer.error('This does not work');
+          }, 2000
+        );
       }
+    );
+
+    myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('complete'); },
+    );
+
+
+    const myObservable2 = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout(
+          () => {
+            observer.next('First package');
+          }, 2000
+        );
+
+        setTimeout(
+          () => {
+            observer.complete();
+          }, 2000
+        );
+
+        setTimeout(
+          () => {
+            observer.next('Second package');
+          }, 2000
+        );
+      }
+    );
+
+// Subscribe with the data and complete arguments.
+    myObservable2.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('complete'); },
     );
   }
 
