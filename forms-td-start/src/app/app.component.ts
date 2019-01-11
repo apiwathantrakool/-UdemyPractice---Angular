e60be5f6574ec7582bce83e58b1b08223454ac88
apiwat.h <apiwat.h@aware.co.th>
2019-01-11 14:56:51 +0700

    Section-15-179: @ViewChild.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,14 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('dataForm') dataForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   onSubmit(dataForm: NgForm) {
     console.log(dataForm );
+    console.log(this.dataForm );
   }
 }
