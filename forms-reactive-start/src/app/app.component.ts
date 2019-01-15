1626d0f8f9b93a708a8ef0c23eb1c647fbd0f97d
apiwat.h <apiwat.h@aware.co.th>
2019-01-15 11:50:48 +0700

    Section-15-Part2-200: Custom Validator.

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddennNames.bind(this)]),
         'email': new FormControl(null, [ Validators.required, Validators.email ])
       }),
       'gender': new FormControl('male'),
@@ -32,5 +33,13 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     // <FormArray> to tell the TS that this value is the FormArray type.
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddennNames(control: FormControl): {[s: string]: boolean} {
+    // -1 mean value is not part of forbiddenUsernames.
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden' : true};
+    }
+    return null;
   }
 }
