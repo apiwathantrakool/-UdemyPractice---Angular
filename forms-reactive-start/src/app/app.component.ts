af74b6d7aca2a962bdb97892a0336921c19e735e
apiwat.h <apiwat.h@aware.co.th>
2019-01-15 13:55:28 +0700

    Section-15-Part2-202: Custom Validators (Promise).

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +15,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
+        // .bind(this) use if there is call some value "this." inside the function.
+        // Because JS will not no who call the value which is inside the called function.
         'username': new FormControl(null, [Validators.required, this.forbiddennNames.bind(this)]),
-        'email': new FormControl(null, [ Validators.required, Validators.email ])
+        'email': new FormControl(null, [ Validators.required, Validators.email ], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([
@@ -37,9 +40,28 @@ export class AppComponent implements OnInit {
 
   forbiddennNames(control: FormControl): {[s: string]: boolean} {
     // -1 mean value is not part of forbiddenUsernames.
+    // This function call the "this.forbiddenUsernames".
+    // So it need to ".bind(this)".
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden' : true};
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>(
+      (resolve, reject) => {
+        setTimeout(
+          () => {
+            if (control.value === 'test@test.com') {
+              resolve({'emilIsForbidden': true});
+            } else {
+              resolve(null);
+            }
+          }, 1500
+        );
+      }
+    );
+    return promise;
   }
 }
