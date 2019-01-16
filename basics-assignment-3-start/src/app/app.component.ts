8b7ca20e20984c7f61b70db2b2be7c9fffa83f8e
apiwat.h <apiwat.h@aware.co.th>
2019-01-16 17:58:50 +0700

    Assignment-3: Complete

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  detail: String = 'Hello World';
+  showDetail: Boolean = false;
+  count = 0;
+  countList = [];
+
+  onClick() {
+    this.count++;
+    this.countList.push(this.count);
+    this.showDetail = !this.showDetail;
+    console.log(this.count);
+  }
 }
