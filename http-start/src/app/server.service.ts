0a74f273b6690c71c600608850a4bdfa0e593a9f
apiwat.h <apiwat.h@aware.co.th>
2019-01-16 10:11:54 +0700

    Section-18-242: Set Headers.

--- http-start/src/app/server.service.ts
+++ http-start/src/app/server.service.ts
@@ -6,8 +6,11 @@ export class ServerService {
     constructor(private http: Http) {}
 
     storeServers(servers: any[]) {
+        const headers = new Headers({'Content-Type': 'application/json'});
+
         // add "data.json" to send the json.
-        return this.http.post('https://angular-training-d4693.firebaseio.com/data.json', servers);
+        return this.http.post('https://angular-training-d4693.firebaseio.com/data.json', servers,
+            {headers : headers});
     }
 
 }
