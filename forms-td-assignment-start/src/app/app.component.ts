e5093eea032674de891594651a7a4fbd645a9ee4
apiwat.h <apiwat.h@aware.co.th>
2019-01-22 10:16:56 +0700

    Assignment-6: Complete (Bad logic, but just for show about ViewChild with ngModel tag.)

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isFormValid = false;
+  isSubmitted = false;
+  @ViewChild('email') emailRef: NgModel;
+  @ViewChild('password') passwordRef: NgModel;
+  emailWarningMessage = '';
+  passwordWarningMessage = '';
+  userData = {
+    email: '',
+    password: '',
+    level: 'Advance'
+  };
+  submitUserData = {
+    email: '',
+    password: '',
+    level: ''
+  };
+  levelOptions = [
+    'Basic',
+    'Advance',
+    'Pro'
+  ];
+  onSubmit(dataForm: NgForm) {
+    console.log(dataForm);
+    this.isSubmitted = true;
+    this.setEmailWarningMessage(this.emailRef);
+    this.setPasswordWarningMessage(this.passwordRef);
+    this.submitUserData.email = this.userData.email;
+    this.submitUserData.password = this.userData.password;
+    this.submitUserData.level = this.userData.level;
+  }
 
+  setEmailWarningMessage(emailRef: NgModel) {
+    if (!emailRef.valid && this.isSubmitted) {
+      this.emailWarningMessage = 'Input Email';
+    }
+    if (!emailRef.valid && this.emailRef.touched) {
+      this.emailWarningMessage = 'Invalid Email';
+    }
+  }
+
+  setPasswordWarningMessage(passwordRef: NgModel) {
+    if (!passwordRef.valid && this.isSubmitted) {
+      this.passwordWarningMessage = 'Input Password';
+    }
+    if (!passwordRef.valid && this.passwordRef.touched) {
+      this.passwordWarningMessage = 'Invalid Password';
+    }
+  }
 }
